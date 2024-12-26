58504e61d3d960fc024edea5df2a130fa1ccdf79
Krzysztof <krzysztof@engineer.com>
2024-12-26 19:46:20 +0100

    add static styles

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 	"log/slog"
 	"net/http"
 	"template/view"
 )
 
+var (
+	//go:embed static
+	staticFS embed.FS
+)
+
 func main() {
 	r := http.NewServeMux()
 
 	t := view.Must("layouts/base.html", "pages/home.html")
-	r.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, nil)
 	})
+
+	staticSubDir, _ := fs.Sub(staticFS, "static")
+	fs := http.FileServerFS(staticSubDir)
+
+	r.Handle("/", http.StripPrefix("/static/", fs))
 
 	s := http.Server{
 		Addr:    ":4000",
